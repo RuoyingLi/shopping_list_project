0bdc9e41888677e77b3f00d4e79fad616dd04d34
ireneli <ler>
2018-12-26 13:52:51 +0100

    comment

--- GoGou/app/src/main/java/com/example/ruoying/gogou/MainActivity.java
+++ GoGou/app/src/main/java/com/example/ruoying/gogou/MainActivity.java
@@ -4,7 +4,7 @@ package com.example.ruoying.gogou;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
-
+/**********************************/
 
 
 public class MainActivity extends AppCompatActivity {
